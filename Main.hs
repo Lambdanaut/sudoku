4208b592f22ce69676eb38353dbb6ec7fbc77542
lambdanaut <personettelabs@gmail.com>
2014-03-16 11:12:58 -0700

    Removes the state monad because wtf am I even using it for. I have lazy evaluation!

--- Main.hs
+++ Main.hs
@@ -39,50 +39,34 @@ heuristic puzzle = V.length $ V.filter isNothing puzzle
 --         * the frontier is empty, in which case the puzzle is unsolvable
 --  3) Return the final state
 
-data SolveState = SolveState {
-	  ss_puzzle   :: Puzzle
-	, ss_explored :: V.Vector Puzzle
-	, ss_frontier :: V.Vector Puzzle
-} deriving Show
-
--- Gets the value from running the stateful _solve function
+-- Takes an unsolved puzzle. Returns a solved puzzle
 solve :: Puzzle -> Puzzle
-solve starting_puzzle = fst $ runState _solve solve_state
-        -- The beginning state for the problem. Adds the current puzzle to the frontier. 
-  where solve_state = SolveState starting_puzzle V.empty (V.singleton starting_puzzle)
+-- The beginning state for the problem. Adds the current puzzle to the frontier. 
+solve puzzle = _solve puzzle V.empty (V.singleton puzzle)
 
-_solve :: State SolveState Puzzle
-_solve = do
-	-- Load state
-	state <- get
-	let frontier = ss_frontier state
-
+_solve :: Puzzle -> V.Vector Puzzle -> V.Vector Puzzle -> Puzzle
+_solve puzzle explored frontier = V.fromList []
+  where
 	-- TODO: Fail if the frontier is empty
 
 	-- Get the best scoring heuristic on the frontier. This is our new current state.
-	let frontier_h = V.map (\puzzle -> (puzzle, heuristic puzzle) ) frontier
-	let lowest_h = fst $ V.minimumBy (\(_, h1) (_, h2) -> compare h1 h2) frontier_h
+	frontier_h = V.map (\puzzle -> (puzzle, heuristic puzzle) ) frontier
+	lowest_h = fst $ V.minimumBy (\(_, h1) (_, h2) -> compare h1 h2) frontier_h
 
 	-- Switch the current puzzle to the explored list
-	let explored = V.cons lowest_h explored
-	let frontier = V.filter (== lowest_h) frontier  -- Very inefficient. Should just remove an index instead of filtering for a puzzle.
+	explored = V.cons lowest_h explored
+	frontier = V.filter (== lowest_h) frontier  -- Very inefficient. Should just remove an index instead of filtering for a puzzle.
 
 	-- Build a list of all possible legal board moves
-	let magic_grid = [(row, column) | column <- [0..8], row <- [0..8] ]
-	let possible_moves = map (\(row, column) -> legal_moves lowest_h row column) magic_grid
+	magic_grid = [(row, column) | column <- [0..8], row <- [0..8] ]
+	possible_moves = map (\(row, column) -> legal_moves lowest_h row column) magic_grid
 
 	-- Add future state data to all possible moves
 
 	-- Filter out all moves that lead to states that have already been explored
 
-
-
-	-- Update the state
-	put $ state { ss_puzzle = lowest_h }
-
-
-	return $ V.fromList []
-
+	-- Continue to the next state
+	--_solve lowest_h NEW_EXPLORED_HERE NEW_FRONTIER_HERE
 
 
 solved :: Puzzle -> Bool
@@ -108,11 +92,6 @@ legal_moves puzzle row column
 	square_column    = get_column puzzle column
 	square_neighbors = map unJust $ filter isJust $ V.toList $ square_row V.++ square_column
 
--- Util
-unJust :: Square -> Int
-unJust (Just x) = x
-unJust Nothing  = 0
-
 get_index :: Puzzle -> Int -> Int -> Square
 get_index puzzle row column = puzzle V.! (row * 9 + column)
 
@@ -136,6 +115,11 @@ load_puzzle_ = load_puzzle puzzle_file
 
 parse_puzzle :: String -> Puzzle
 parse_puzzle unparsed = V.fromList $ fmap (\x-> readMay [x]) unparsed
+
+-- Util function
+unJust :: Square -> Int
+unJust (Just x) = x
+unJust Nothing  = 0
 
 main :: IO ()
 main = do
